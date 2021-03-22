9fd6e346ddd8119b0c04e37ac18b29e7a2247c21
Amos Peltonen <amos.peltonen@gmail.com>
2021-03-22 10:09:21 +0100

    Asset Loader now loads assets in pararell

--- src/Density/Assets.js
+++ src/Density/Assets.js
@@ -1,34 +1,49 @@
+import { sleep } from "./Core.js";
+
 // Density Asset Loader
 var assets = {};
 assets.isDone = false;
 loadDefaultAssets();
-function loadDefaultAssets(done = (e) => {}, itemload = (e) => {}) {
+function loadDefaultAssets(
+  done = (e) => {
+    console.log("Assets Loaded!");
+  }
+) {
   var assetContainer = document.createElement("DIV");
   assetContainer.style.display = "none";
   var http = new XMLHttpRequest();
   http.onload = async () => {
+    var assetsLoaded = 0;
+    var totalAssets = 0;
     var assetsData = http.response;
     for (let i = 0; i < assetsData.length; i++) {
       const element = assetsData[i];
       if (element.type == undefined) {
+        totalAssets++;
         const imgElement = document.createElement("IMG");
-        itemload(element);
         imgElement.src = "./assets/" + element;
         assetContainer.append(imgElement);
-        await waitForImageLoad(imgElement);
+        imgElement.onload = () => {
+          assetsLoaded++;
+        };
         assets[element.split(".")[0].replace(/\//g, "_")] = imgElement;
       } else if (element.type == "animation") {
         const arr = [];
         for (let item = 1; item < element.itemcount; item++) {
+          totalAssets++;
           const imgElement = document.createElement("IMG");
-          itemload(element);
           imgElement.src = "./assets/" + element.path + item + ".png";
           assetContainer.append(imgElement);
-          await waitForImageLoad(imgElement);
+          imgElement.onload = () => {
+            assetsLoaded++;
+          };
           arr.push(imgElement);
         }
         assets[element.path.split(".")[0].replace(/\//g, "_")] = arr;
       }
+    }
+    while (assetsLoaded != totalAssets) {
+      await sleep(500);
     }
     done();
     assets.isDone = true;
@@ -36,27 +51,6 @@ function loadDefaultAssets(done = (e) => {}, itemload = (e) => {}) {
   http.responseType = "json";
   http.open("GET", "./assets/assets.json");
   http.send();
-}
-
-async function loadAssets(assetsData, done, itemload) {
-  var assetContainer = document.createElement("DIV");
-  assetContainer.style.display = "none";
-  for (let i = 0; i < assetsData.length; i++) {
-    const element = assetsData[i];
-    const imgElement = document.createElement("IMG");
-    itemload(element);
-    imgElement.src = "./assets/" + element;
-    assetContainer.append(imgElement);
-    await waitForImageLoad(imgElement);
-    assets[element.replace(/\//g, "_")] = imgElement;
-  }
-  done();
-}
-
-async function waitForImageLoad(img) {
-  return new Promise((resolve, reject) => {
-    img.onload = resolve;
-  });
 }
 
 export default assets;
